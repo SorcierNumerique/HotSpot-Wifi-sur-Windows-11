28a1537eb38996d1a5a71b91733abb7bbadd0212
SorcierNumerique <158748099+SorcierNumerique@users.noreply.github.com>
2024-06-08 19:04:41 +0200

    Add files via upload

--- test_network.bat
+++ test_network.bat
@@ -3,7 +3,7 @@ netsh wlan show drivers | findstr /c:"Hosted network supported  : Yes" >nul
 if %errorlevel%==0 (
     echo Hosted network supported
 ) else (
-    echo "Le programme ne peut pas s'exécuter correctement car votre ordinateur ne prend pas en charge la fonctionnalité de réseau hébergé."
+    echo "Le programme ne peut pas s'executer correctement car votre ordinateur ne prend pas en charge la fonctionnalite de reseau heberge."
     echo Hosted network not supported
 )
 pause
