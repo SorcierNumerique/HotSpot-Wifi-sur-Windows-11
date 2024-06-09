cc3dd19c81f6b9567fcd9a6aabf437a770e5999e
SorcierNumerique <158748099+SorcierNumerique@users.noreply.github.com>
2024-06-09 11:16:08 +0200

    Update test carte wifi.bat

--- test carte wifi.bat
+++ test carte wifi.bat
@@ -1,10 +1,11 @@
+REM Ce fichier batch permet de vérifier que votre carte WiFi est compatible pour le réseau hébergé.
 @echo off
 netsh wlan show drivers | findstr /c:"Hosted network supported  : Yes" >nul
 if %errorlevel%==0 (
     echo Hosted network supported
 ) else (
     echo "Le programme ne peut pas s'executer correctement car votre ordinateur ne prend pas en charge la fonctionnalite de reseau heberge."
-    echo Hosted network not supported
+    echo Le réseau heberge n'est pas pris en charge
 )
 pause
 exit
