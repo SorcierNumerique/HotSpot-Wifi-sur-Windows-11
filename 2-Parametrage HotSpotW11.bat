2c3ec49af24e165ea0be71d66b2049408e278cf8
SorcierNumerique <158748099+SorcierNumerique@users.noreply.github.com>
2024-06-09 11:21:45 +0200

    Update and rename param_network.bat to 2-Parametrage HotSpotW11.bat

--- 2-Parametrage HotSpotW11.bat
+++ 2-Parametrage HotSpotW11.bat
@@ -1,3 +1,4 @@
+REM Il est conseillé de changer le paramètre key qui est le mot de passe de votre HotSpot
 @echo off
 netsh wlan set hostednetwork mode=allow ssid=HotSpotW11 key=11Notepad18@#!
 pause
